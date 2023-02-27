a6f2417d028f7e4764e17c4afa18bcbf3a828a3e
Isaac Alencar <alencarisaac3@gmail.com>
2023-02-27 14:33:16 -0300

    create todos resources and views for index resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  get '/', to: 'example#index'
+  root 'todos#index'
+  # get '/', to: 'example#index'
+  resources :todos, only: %i[index create update]
 end
